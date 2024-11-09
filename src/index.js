d1707bc1f15223ffa68600df84fc9debf58ce298
Atul72 <rai703537@gmail.com>
2024-11-09 23:40:28 +0530

    final

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={40} color="red" className="" defaultRating={0} />
+    <StarRating size={50} color="pink" className="" defaultRating={1} />
+    <Test /> */}
+  </React.StrictMode>
+);
